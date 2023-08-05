9c06e1794f23bd77eb845e5708e7b2e5b014eeb7
AICatgirls <130926942+AICatgirls@users.noreply.github.com>
2023-08-05 16:14:28 -0700

    Add files via upload

--- aichatgirls.py
+++ aichatgirls.py
@@ -1,14 +1,20 @@
 # TODO: try/except on requests, handle disconnects
-# TODO: when the chat_history gets long oobabooga cuts off the context; make sure it gets preserved
-import discord
-import requests
-import os
+# TODO: Allow user configurable max response length,
+#       Send multiple messages if it exceeds discord's 2000 character length
+# TODO: Make temperature a user configurable setting
+# TODO: Tokenize user messages to get an exact count of tokens rather than estimating it based on length
+# TODO: Command ideas: regen, undo
+
 import asyncio
-from datetime import datetime
-import chatHistory
 import chatCommand
-from loadCharacterCard import load_character_card
+import chatHistory
+from datetime import datetime
+import discord
 from dotenv import load_dotenv
+from loadCharacterCard import load_character_card
+import os
+import requests
+
 load_dotenv()
 
 MODEL_MAX_TOKENS = 8000
@@ -44,57 +50,59 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    print(message)
-    if message.content:
-        if message.content.startswith("!"):
-            command = message.content.split(" ")[0]
-            text_response = chatCommand.chat_command(command, message, character)
-            if isinstance(message.channel, discord.DMChannel):
-                await message.author.send(text_response)
-            else:
-                await message.channel.send(text_response)
-        elif message.author == client.user:
-            return
-        else:
-            chat_history = chatHistory.load_chat_history(message.author, character)
-
-            # Send the prompt to the API endpoint and get the response
-            prompt = (context + "\n" + chat_history[-MAX_CHAT_HISTORY_LENGTH:] + "\n" +
-                f"{message.author.display_name}: {message.content}\n" +
-                f"{character.name}: ")
-            print(prompt)
-            stopping_strings = [f"{message.author.display_name}:"]
-            response = requests.post(
-                API_ENDPOINT,
-                headers=headers,
-                json={
-                    "prompt": prompt,
-                    "max_length": 400,
-                    "stopping_strings": stopping_strings
-                }
-            )
-            
-            # Keep checking if response has been received
-            response_json = None
-            while not response_json:
-                # Show "typing" status while generating response
-                async with message.channel.typing():
-                    response_json = response.json()
-                    if len(response_json["results"]) > 0:
-                        text_response = response_json["results"][0]["text"]
-                    else:
-                        text_response = "Sorry, I couldn't generate a response."
-                    await asyncio.sleep(1) # wait for 1 second before checking again
-
-            # Append the original message and text response to the chat history
-            chat_history += f"\n{message.author.name}: {message.content}\n{character.name}: {text_response}"
-            chatHistory.save_chat_history(chat_history)
-
-            if isinstance(message.channel, discord.DMChannel):
-                await message.author.send(text_response)
-            else:
-                await message.channel.send(text_response)
-    else:
+    if message.author == client.user:
         return
+
+    # Check if it's a command
+    if message.content.startswith("!"):
+        command = message.content.split(" ")[0]
+        text_response = await handle_command(command, message)
+    else:
+        text_response = await generate_prompt_response(message)
+
+    if isinstance(message.channel, discord.DMChannel):
+        await message.author.send(text_response)
+    else:
+        await message.channel.send(text_response)
+
+async def handle_command(command, message):
+    return chatCommand.chat_command(command, message, character)
+
+async def generate_prompt_response(message):
+    chat_history = chatHistory.load_chat_history(message.author, character)
+    prompt = (context + "\n" + chat_history[-MAX_CHAT_HISTORY_LENGTH:] + "\n" +
+              f"{message.author.display_name}: {message.content}\n" +
+              f"{character.name}: ")
+
+    stopping_strings = [f"{message.author.display_name}:"]
+    response = requests.post(
+        API_ENDPOINT,
+        headers=headers,
+        json={
+            "prompt": prompt,
+            "max_new_tokens": 400,
+            "temperature": 0.5,
+            "repetition_penalty": 1.18,
+            "stopping_strings": stopping_strings
+        }
+    )
+
+    # Keep checking if response has been received
+    response_json = None
+    while not response_json:
+        # Show "typing" status while generating response
+        async with message.channel.typing():
+            response_json = response.json()
+            if len(response_json["results"]) > 0:
+                text_response = response_json["results"][0]["text"]
+            else:
+                text_response = "Sorry, I couldn't generate a response."
+            await asyncio.sleep(1) # wait for 1 second before checking again
+
+    # Append the original message and text response to the chat history
+    chat_history += f"\n{message.author.name}: {message.content}\n{character.name}: {text_response}"
+    chatHistory.save_chat_history(chat_history)
+
+    return text_response
         
 client.run(DISCORD_TOKEN)
